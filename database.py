b9d292c7bba694f26abf696aa997efa603449be5
Matias Cardone <matias.cardone@axelspringer.com>
2025-04-29 08:45:26 +0200

    Create DB local and production environment.

--- database.py
+++ database.py
@@ -1,9 +1,17 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
+import dotenv
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/newsdb")
+dotenv.load_dotenv()
+
+ENV = os.getenv("ENV", "local")
+
+if ENV == "local":
+    DATABASE_URL = os.getenv("LOCAL_DATABASE_URL", "postgresql://postgres:postgres@localhost:5433/newsdb")
+else:
+    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/newsdb")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
