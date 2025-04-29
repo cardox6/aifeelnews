fced1b325ad1a1fa87d403ea31f98dc5c59baf42
Matias Cardone <matias.cardone@axelspringer.com>
2025-04-29 12:04:59 +0200

    Update routers with Sources

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import users, articles, bookmarks
+from routers import users, articles, bookmarks, sources
 from database import Base, engine
-import models.article, models.user, models.bookmark
+import models.article, models.user, models.bookmark, models.source
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI(title="aiFeelNews API")
@@ -20,6 +20,7 @@ app.add_middleware(
 app.include_router(users.router, prefix="/users", tags=["Users"])
 app.include_router(articles.router, prefix="/articles", tags=["Articles"])
 app.include_router(bookmarks.router, prefix="/bookmarks", tags=["Bookmarks"])
+app.include_router(sources.router, prefix="/sources", tags=["Sources"])
 
 @app.get("/")
 def root():
